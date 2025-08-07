01257bad3345e6ccb770fdcb480ec7c1e1ae558e
Zero <15217555+zero-adapter@user.noreply.gitee.com>
2025-08-07 08:13:16 +0800

    1.1.2!

--- overlay_window.py
+++ overlay_window.py
@@ -341,7 +341,7 @@ class OverlayWindow:
                 item('校准配置', self._create_profile_submenu()),
                 item('帧数显示', self._create_display_mode_submenu()),
                 Menu.SEPARATOR,
-                item(f'v1.1.2? Z_06作品', self._open_about_page),
+                item(f'v1.1.2 Z_06作品', self._open_about_page),
                 item('退出', self._schedule_quit)
             )
             logger.debug("托盘菜单已更新。")
@@ -387,7 +387,7 @@ class OverlayWindow:
                 item('校准配置', self._create_profile_submenu()),
                 item('帧数显示', self._create_display_mode_submenu()),
                 Menu.SEPARATOR,
-                item(f'v1.1.2? Z_06作品', self._open_about_page),
+                item(f'v1.1.2 Z_06作品', self._open_about_page),
                 item('退出', self._schedule_quit)
             )
 
